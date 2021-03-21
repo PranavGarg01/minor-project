2b39b6a96ee1eb846c7f648ca0c4aa05a2248aef
shreyanshi dhir <shreyanshidhir18@gmail.com>
2021-03-22 01:59:21 +0530

    Register page added

--- frontend/src/views/FormRegister.js
+++ frontend/src/views/FormRegister.js
@@ -3,7 +3,7 @@ import validate from "./validateInfo";
 import useForm from "./useForm";
 import "./Form.css";
 
-const FormSignup = ({ submitForm }) => {
+const FormRegister = ({ submitForm }) => {
 	const { handleChange, handleSubmit, values, errors } = useForm(
 		submitForm,
 		validate
@@ -12,8 +12,22 @@ const FormSignup = ({ submitForm }) => {
 	return (
 		<div className="form-content-right">
 			<form onSubmit={handleSubmit} className="form" noValidate>
-				<h1>Login with your already existing account !</h1>
-
+				<h1>
+					Get started with us today! Create your account by filling
+					out the information below.
+				</h1>
+				<div className="form-inputs">
+					<label className="form-label">Username</label>
+					<input
+						className="form-input"
+						type="text"
+						name="username"
+						placeholder="Enter your username"
+						value={values.username}
+						onChange={handleChange}
+					/>
+					{errors.username && <p>{errors.username}</p>}
+				</div>
 				<div className="form-inputs">
 					<label className="form-label">Email</label>
 					<input
@@ -38,13 +52,27 @@ const FormSignup = ({ submitForm }) => {
 					/>
 					{errors.password && <p>{errors.password}</p>}
 				</div>
-
+				<div className="form-inputs">
+					<label className="form-label">Confirm Password</label>
+					<input
+						className="form-input"
+						type="password"
+						name="password2"
+						placeholder="Confirm your password"
+						value={values.password2}
+						onChange={handleChange}
+					/>
+					{errors.password2 && <p>{errors.password2}</p>}
+				</div>
 				<button className="form-input-btn" type="submit">
-					Login
+					Sign up
 				</button>
+				<span className="form-input-login">
+					Already have an account? Login <a href="#">here</a>
+				</span>
 			</form>
 		</div>
 	);
 };
 
-export default FormSignup;
+export default FormRegister;
--- frontend/src/views/Register.js
+++ frontend/src/views/Register.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Form.css";
+import FormSignup from "./FormRegister";
+import FormSuccess from "./FormSuccess";
 
-const Register = () => {
+const Form = () => {
+	const [isSubmitted, setIsSubmitted] = useState(false);
+
+	function submitForm() {
+		setIsSubmitted(true);
+	}
 	return (
-		<div>
-			Register Page
-		</div>
-	)
-}
+		<>
+			<div className="form-container">
+				<span className="close-btn">×</span>
+				<div className="form-content-left">
+					<img
+						className="form-img"
+						src="img/img-2.png"
+						alt="spaceship"
+					/>
+				</div>
+				{!isSubmitted ? (
+					<FormSignup submitForm={submitForm} />
+				) : (
+					<FormSuccess />
+				)}
+			</div>
+		</>
+	);
+};
 
-export default Register
+export default Form;
