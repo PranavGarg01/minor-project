27ff41e73d7f6709ccb0f6b2c5fb8663b590e583
shreyanshi dhir <shreyanshidhir18@gmail.com>
2021-03-24 02:02:55 +0530

    Navabar Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import {Switch,Link,BrowserRouter,Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
-import Home from './views/Home'
-import Login from './views/Login';
-import Register from './views/Register';
-const App =()=> 
-{
-  return(
-  <BrowserRouter>
-  {/* <Navbar/> */}
-    <Switch>
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route exact path="/login">
-        <Login/>
-      </Route>
-      <Route exact path="/register">
-        <Register/>
-      </Route>
-    </Switch>
-  </BrowserRouter>
-  )
-}
+import { Switch, Link, BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.js";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import "./App.css";
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/login">
+					<Login />
+				</Route>
+				<Route exact path="/register">
+					<Register />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
+};
 
 export default App;
