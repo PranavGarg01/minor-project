7c9904f977595325e12594de6f4a8942e2aa684b
shreyanshi dhir <shreyanshidhir18@gmail.com>
2021-04-09 22:09:36 +0530

    homepage done

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -6,26 +6,7 @@ const Home = () => {
 	return (
 		<body>
 			<section class="banner">
-				<Grid container>
-					<Grid item xs={12} md={4}>
-						<div></div>
-					</Grid>
-					<Grid item xs={12} md={8}>
-						<div>
-							<img
-								style={{
-									marginRight: "0",
-									marginLeft: "auto",
-									display: "block",
-									maxWidth: "50rem",
-									maxHeight: "50rem",
-								}}
-								src="img/img-4.png"
-								alt="main-image"
-							/>
-						</div>
-					</Grid>
-				</Grid>
+				<div></div>
 			</section>
 		</body>
 	);
